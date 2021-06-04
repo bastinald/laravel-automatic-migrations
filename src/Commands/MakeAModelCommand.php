43683375bf2a655cebad13e0be9727c709084a0d
bastinald <bastinald@icloud.com>
2021-06-04 13:38:55 -0400

    model cmd tweaks

--- src/Commands/MakeAModelCommand.php
+++ src/Commands/MakeAModelCommand.php
@@ -8,40 +8,20 @@ use Illuminate\Support\Str;
 class MakeAModelCommand extends ModelMakeCommand
 {
     protected $name = 'make:amodel';
-    protected $studlyName;
 
     public function handle()
     {
-        $this->studlyName = Str::studly($this->argument('name'));
-
-        $this->renameUserMigration();
-        $this->createFactory();
+        $this->call('make:afactory', [
+            'name' => Str::studly($this->argument('name')),
+            '--force' => $this->option('force'),
+        ]);
 
         return parent::handle();
     }
 
-    protected function renameUserMigration()
-    {
-        if ($this->studlyName == 'User') {
-            $migration = database_path('migrations/2014_10_12_000000_create_users_table.php');
-
-            if (file_exists($migration)) {
-                rename($migration, $migration . '.bak');
-            }
-        }
-    }
-
-    protected function createFactory()
-    {
-        $this->call('make:afactory', [
-            'name' => $this->studlyName,
-            '--force' => $this->option('force'),
-        ]);
-    }
-
     protected function getStub()
     {
-        $stub = $this->studlyName == 'User' ? 'user-model' : 'model';
+        $stub = Str::studly($this->argument('name')) == 'User' ? 'user-model' : 'model';
 
         return rtrim(config('laravel-automatic-migrations.stub_path'), '/') . '/' . $stub . '.stub';
     }
