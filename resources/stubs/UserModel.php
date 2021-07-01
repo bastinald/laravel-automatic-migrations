ece4ab3e8a27b31c1119698237094171b219ade0
bastinald <bastinald@icloud.com>
2021-07-01 06:14:16 -0400

    ocd

--- resources/stubs/UserModel.php
+++ resources/stubs/UserModel.php
@@ -24,10 +24,10 @@ class DummyModelClass extends Authenticatable
         $table->id();
         $table->string('name');
         $table->string('email')->unique();
-        $table->timestamp('email_verified_at')->nullable();
         $table->string('password');
         $table->rememberToken();
         $table->string('timezone')->nullable();
+        $table->timestamp('email_verified_at')->nullable();
         $table->timestamp('created_at')->nullable();
         $table->timestamp('updated_at')->nullable();
     }
@@ -37,10 +37,10 @@ class DummyModelClass extends Authenticatable
         return [
             'name' => $faker->firstName,
             'email' => $faker->unique()->safeEmail,
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'timezone' => $faker->timezone,
+            'email_verified_at' => now(),
             'created_at' => $faker->dateTimeBetween(now()->subMonth(), now()),
         ];
     }
