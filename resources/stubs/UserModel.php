7269c6218c11a5e301c26fc4ff5a935a2b5c9e11
bastinald <bastinald@icloud.com>
2021-07-01 05:54:47 -0400

    faker tz

--- resources/stubs/UserModel.php
+++ resources/stubs/UserModel.php
@@ -40,6 +40,7 @@ class DummyModelClass extends Authenticatable
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'timezone' => $faker->timezone,
             'created_at' => $faker->dateTimeBetween(now()->subMonth(), now()),
         ];
     }
